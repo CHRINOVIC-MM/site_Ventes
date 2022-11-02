dc781a6785b278a95abf2f3c92594453edab1dee
Chrinovic MM <20mm364@esisalama.org>
2022-11-02 11:00:57 +0200

    Ajout du bouton close

--- js/app.js
+++ js/app.js
@@ -6,3 +6,16 @@ menuBarsBtn.addEventListener("click", ()=>{
     menuBarsBtn.classList.toggle("fa-times");
     navBar.classList.toggle("active");
 });
+
+window.onscroll =  ()=>{
+    menuBarsBtn.classList.remove("fa-times");
+    navBar.classList.remove("active");
+}
+
+document.querySelector("#search-icon").onclick = ()=>{
+    document.querySelector("#search-form").classList.toggle("active");
+}
+
+document.querySelector("#close").onclick = ()=>{
+    document.querySelector("#search-form").classList.remove("active");
+}
